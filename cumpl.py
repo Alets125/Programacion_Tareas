7185c1fd1d8350d8d4b20d59c04554977cd48fee
Alets125 <jesusalexis-115@hotmail.com>
2024-09-11 00:07:24 -0700

    Add files via upload
    Aun me faltan otros codigos por enviar

---
+++ cumpl.py
@@ -0,0 +1,11 @@
+
+hoy = input("Por favor, ingrese la fecha de hoy (MMDD): ")
+
+# Obtener el cumpleaños del usuario
+cumplea_usuario = input("Por favor, ingrese su cumpleaños (MMDD): ")
+
+# Verificar si el cumpleaños del usuario es hoy
+if cumplea_usuario == hoy:
+    print("¡Feliz cumpleaños! Espero que tengas un día maravilloso!")
+else:
+    print("Hoy no es tu cumpleaños, pero espero que tengas un gran día de todos modos!")
